7500dd506dfdde38741b5e20980b844202cca6d9
yaoyuan4102 <yaoyuan4102@gmail.com>
2019-03-07 06:32:31 -0600

    doc-translate: sidebar content translation

--- dev/src/docs/sidebar_content.js
+++ dev/src/docs/sidebar_content.js
@@ -20,24 +20,24 @@ let home = {};
 export default home;
 
 home.id = "home";
-home.label = "Menu";
+home.label = "菜单";
 home.desc = "[from HTML]";
 home.kids = [
 
 	
 	{
-	label: "Pattern Settings",
+	label: "模式设置",
 	id: "share",
 	el: "#share_main",
 	list: false,
 	kids: [
 		{
-		label: "Save to my Favorites", 
+		label: "保存到我的收藏夹",
 		id: "share_favorites",
 		el:"#share_favorites"
 		},
 		{
-		label: "Share with the Community",
+		label: "社区分享",
 		id: "share_community",
 		el:"#share_community"
 		}
@@ -45,16 +45,16 @@ home.kids = [
 	},
 	
 	{
-	label: "My Patterns",
+	label: "我的模式",
 	id:"favorites",
-	desc: "The list above will display any patterns that you create or favorite."+
-		"<p>To edit a pattern you created, click its URL or double-click it in the list to load it, then open Save / Share to edit and save.</p>",
+	desc: "上面的列表将显示您创建或收藏的所有模式。"+
+		"<p>要编辑您创建的模式，请单击其URL或在列表中双击它以加载它，然后打开“保存/共享”进行编辑和保存。</p>",
 	search: true,
 	kids: []
 	},
 	
 	{
-	label: "Cheatsheet",
+	label: "备忘单",
 	id:"cheatsheet",
 	el: "#cheatsheet"
 	},
@@ -64,132 +64,132 @@ home.kids = [
 	},
 	
 	{
-	label: "Community Patterns",
+	label: "模式社区",
 	id: "community",
-	desc: "Welcome to Community Patterns, a searchable database of patterns submitted by users like you."+
-		"<p>After selecting a pattern, click its URL or double-click it in the list to load the full pattern. Or use the right arrow icon to load just the expression or text.</p>"+
-		"<p>Help make the Community better by rating patterns, and submitting your own via <b>Search & Share</b> in the menu.</p>",
+	desc: "欢迎使用模式社区，这是一个由您这样的用户提交的可搜索的模式数据库。"+
+		"<p>选择模式后，单击其URL或在列表中双击它以加载完整模式。或使用右箭头图标仅加载表达式或文本。</p>"+
+		"<p>通过为模式打分，并通过菜单中的<b>搜索&分享</b>提交您自己的模式来帮助社区变得更好。</p>",
 	search: true,
 	kids: []
 	},
 
 	{
-	label: "Help",
+	label: "帮助",
 	id: "help",
-	desc: "Help for the RegExr application. See the <b>RegEx Reference</b> for help with Regular Expressions.",
+	desc: "为RegExr提供帮助。请参阅<b>RegEx参考</b>来完善改进正则表达式。",
 	kids: [
 
 		{
-		label:"About",
+		label:"关于",
 		desc:"RegExr v[build-version] ([build-date])."+
-			"<p>Created by <a href='http://twitter.com/gskinner/' target='_blank'>Grant Skinner</a> and the <a href='http://gskinner.com/' target='_blank'>gskinner</a> team, using the <a href='http://createjs.com/' target='_blank'>CreateJS</a> & <a href='http://codemirror.net/' target='_blank'>CodeMirror</a> libraries.</p>"+
-			"<p>You can provide feedback or log bugs on <a href='http://github.com/gskinner/regexr/' target='_blank'>GitHub</a>.</p>"
+			"<p>由 <a href='http://twitter.com/gskinner/' target='_blank'>Grant Skinner</a> 和 <a href='http://gskinner.com/' target='_blank'>gskinner</a> 团队创建, 并使用了 <a href='http://createjs.com/' target='_blank'>CreateJS</a> & <a href='http://codemirror.net/' target='_blank'>CodeMirror</a> 库.</p>"+
+			"<p>您可以在 <a href='http://github.com/gskinner/regexr/' target='_blank'>GitHub</a> 上提供反馈和记录bugs.</p>"
 		},
 		{
-		label:"Getting started",
-		desc:"RegExr provides real-time visual results, syntax highlighting, tooltips, and undo/redo ({{getCtrlKey()}}-Z / Y) so it's easy and fun to explore Regular Expressions."+
-			"<p>Browse through the <b>RegEx Reference</b> and test different tokens to see what they do, then check out <b>Community Patterns</b> to see examples.</p>"+
-			"<p>You can also <b>Save</b> your patterns for later reference, or to share with others. <b>Sign In</b> to ensure you don't lose your patterns.</p>"+
-			"<p>Modify your pattern's details, share it with the <b>Community</b>, or make it private, or delete it in <b>Pattern Settings</b></p>",
+		label:"开始",
+		desc:"RegExr提供了实时可视化结果，语法突出显示，工具提示和撤消/重做（{{getCtrlKey（）}}  -  Z / Y），因此探索正则表达式非常的简单有趣。"+
+			"<p>浏览 <b>RegEx参考</b>，测试不同模式的匹配结果，并从<b>模式社区</b>中查询示例。</p>"+
+			"<p>您还可以<b>保存</b>您的模式以供日后参考, 或与他人分享。<b>登陆</b>以确保您不会丢失您的模式。</p>"+
+			"<p>通过<b>模式设置</b>修改您的模式的详细信息, 分享到<b>社区</b>, 将其设置为私有或者删除。</p>",
 		kids: [
 			{
-			label:"Expression panel",
-			desc:"This is where you enter a regular expression to test. The results in the <b>Text</b> and <b>Tools</b> panel will update as you type."+
-				"Roll over the expression for information on each token."+
-				"<p>The buttons to the right allow you to switch RegEx engines, or edit the expression flags.</p>"
+			label:"表达式面板",
+			desc:"您可以在此处输入要测试的正则表达式。您的输入结果会在<b>文字</b>和<b>工具</b>面板中自动更新。"+
+				"滚动表达式以获取有关每个元字符的信息。"+
+				"<p>右侧的按钮允许您切换RegEx引擎，或编辑表达式字符。</p>"
 			},
 			{
-			label:"Text panel",
-			desc:"This is where you enter text to test your expression against. Drag & drop a text file to load its contents."+
-				"<p>Matches will be highlighted as you type. Roll over a match for information on the match and its capture groups. The match count and execution time are shown in the title bar.</p>"+
-				"<p>Lighter colored caps at the start or end of a line indicate the match continues between lines.</p>"
+			label:"文本面板",
+			desc:"这是您输入文本以测试表达式的位置。 拖放文本文件以加载其内容。"+
+				"<p>输入时将突出显示匹配项。滚动匹配以获取有关匹配及其捕获组的信息。匹配计数和执行时间显示在标题栏中。</p>"+
+				"<p>在行的开头或结尾处的较浅色盖子表示行之间继续匹配。</p>"
 			},
 			{
-			label:"Tools panel",
-			desc:"Click the <b>Tools</b> title bar below the <b>Text</b> panel to show or hide the <b>Tools</b> panel."+
-				"<p>Tools provide different ways of working with or exploring your results.</p>",
+			label:"工具面板",
+			desc:"单击<b>文本</b>面板下方的<b>工具</b>标题栏以显示或隐藏<b>工具</b>面板。"+
+				"<p>工具提供了不同的查询结果的方式。</p>",
 			kids: [
 				{
-				label:"Replace",
+				label:"替换",
 				id: "replace",
-				desc:"The <b>Replace</b> tool replaces matches with a specified string or pattern."+
-					"<p>Matches in the <b>Text</b> panel are replaced by the substitution string & displayed as you type.</p>"+
-					"<p>Substitution tokens and escaped characters are supported, such as <code>\\n</code>, <code>\\t</code> & <code>\\u0009</code>.</p>"+
-					"<p>Roll over tokens for information, and check out the <b>RegEx Reference</b> for more information.</p>"
+				desc:"<b>替换</b>工具用指定的字符串或者模式来替换匹配项。"+
+					"<p>当您输入时，<b>文本</b>面板中的匹配项将被替换字符串所替换。</p>"+
+					"<p>支持替换元字符和转义字符, 例如 <code>\\n</code>, <code>\\t</code> & <code>\\u0009</code>.</p>"+
+					"<p>滚动元字符以获取信息, 并查看 <b>RegEx 参考</b> 获取更多信息。</p>"
 				},
 				{
-				label:"List",
+				label:"列表",
 				id: "list",
-				desc:"The <b>List</b> tool lists all found matches."+
-					"<p>You can specify either a simple delimiter (ex. <code>,</code> or <code>\\n</code>), or use substitution tokens to generate more advanced reports. For example, <code>$1\\n</code> would list all group 1 results (in the JavaScript engine).</p>"+
-					"<p>Escaped characters are supported, such as <code>\\n</code>, <code>\\t</code> & <code>\\u0009</code>.</p>"+
-					"<p>Roll over tokens for information.</p>"
+				desc:"<b>列表</b>工具陈列了所有的匹配项。"+
+					"<p>您可以指定一个简单的分隔符 (ex. <code>,</code> 或 <code>\\n</code>), 或使用替代元字符生成更高级的报告。例如, <code>$1\\n</code> 将会列出所有的组1的结果(JavaScript引擎).</p>"+
+					"<p>支持转义字符, 如<code>\\n</code>, <code>\\t</code> & <code>\\u0009</code>.</p>"+
+					"<p>滚动元字符以获取信息。</p>"
 				},
 				{
-				label:"Details",
+				label:"详细信息",
 				id: "details",
-				desc:"The <b>Details</b> tool displays the full text of a match and its capture groups."+
-					"<p>Click on a highlighted match in the <b>Text</b> panel to display the details for that match.</p>"+
-					"<p>Roll over a group row to highlight that group in your <b>Expression</b>.</p>"
+				desc:"<b>详细信息</b>工具显示匹配项及其捕获组的全文。"+
+					"<p>单击<b>文本</b>面板中突出显示的匹配项以显示该匹配项的详细信息。"+
+					"<p>滚动一个组行以在<b>表达式</b>中突出显示该组。</p>"
 				},
 				{
-				label:"Explain",
+				label:"说明",
 				id: "explain",
-				desc:"The <b>Explain</b> tool displays a detailed breakdown of the <b>Expression</b>."+
-					"<p>Mouse over the explanation to highlight the related tokens in the <b>Expression</b> panel and vice versa.</p>"+
-					"<p>Click an item in the explanation to show more info in the <b>RegEx Reference</b>.</p>"
+				desc:"<b>说明</b>工具显示<b>表达式</b>的详细分类。"+
+					"<p>将鼠标悬停在解释上以突出显示<b>表达式</b>面板中的相关元字符，反之亦然。</p>"+
+					"<p>单击说明中的项目以在<b>RegEx 参考</b>中显示更多信息。</p>"
 				}
 			]
 			},
 			{
-			label:"Menu",
-			desc:"The <b>Menu</b> (this panel) includes <b>Help</b>, a full <b>RegEx Reference</b>, a <b>Cheatsheet</b>, and <b>Pattern Settings</b> features."+
-				"<p>Double-click a selected item in the <b>RegEx Reference</b> to insert it into your <b>Expression</b>. Click the arrow beside an example to load it.</p>"+
-				"<p>The menu also includes searchable <b>Community Patterns</b>, and patterns you've created or favorited in <b>My Patterns</b>.</p>"
+			label:"菜单",
+			desc:"<b>菜单</b>包括<b>帮助</b>, <b>RegEx 参考</b>, <b>备忘表</b>, 和<b>模式设置</b>等功能."+
+				"<p>双击<b>RegEx 参考</b>中的选定项，将其插入<b>表达式</b>。单击示例旁边的箭头以加载。</p>"+
+				"<p>菜单同时包含可搜索的<b>模式社区</b>, 以及您在<b>我的模式</b>中创建或收藏的模式。</p>"
 			}
 		]
 		},
 		{
-		label:"Signing in",
+		label:"登陆",
 		id: "signin",
-		desc:"Before you sign in, RegExr creates a temporary account which relies on a browser cookie. This means you can't access your patterns on other computers, and that you could lose your patterns if your cookies are deleted or expire."+
-			"<p>Signing in creates a permanent account, so you can access your patterns anywhere, anytime.</p>"+
-			"<p>Your existing patterns &amp; favorites will automatically be assigned to the new account.</p>"+
-			"<p>We don't use your info for anything other than signing you into your RegExr account.</p>"
+		desc:"登陆之前, RegExr创建一个依赖于浏览器cookie的临时帐户。这意味着您无法在其他计算机上访问您的模式，并且您可能会丢失模式如果您的cookie被删除或者过期。"+
+			"<p>登录会创建一个永久帐户，因此您可以随时随地访问您的模式。</p>"+
+			"<p>收藏夹将自动分配您已有的模式到新账户中。</p>"+
+			"<p>除了将您登录到您的RegExr帐户之外，我们不会将您的信息用于任何其他用途。</p>"
 		},
 		{
 		id: "engine",
-		label:"RegEx engines",
-		desc:"While the core feature set of regular expressions is fairly consistent, different implementations (ex. Perl vs Java) may have different features or behaviours."+
-			 "<p>RegExr currently supports JavaScript RegExp executed in your browser and PCRE via PHP.</p>"+
-			 "<p>You can switch engines using the dropdown in Expression.</p>",
+		label:"RegEx 引擎",
+		desc:"虽然正则表达式的核心功能集相当一致，但不同的实现（例如Perl与Java）可能具有不同的功能或行为。"+
+			 "<p>RegExr当前支持在浏览器中执行JavaScript RegExp和通过PHP运行PCRE。</p>"+
+			 "<p>您可以使用表达式面板上的下拉列表切换引擎。</p>",
 		kids: [
 			{
 			label:"JavaScript",
-			desc:"Your browser's JavaScript engine is used to execute RegEx in an asynchronous worker using <code>RegExp.exec()</code>."+
-				"<p>Note that while implementations are mostly consistent, there are small variations between browsers. Here is a short list of known differences:<ul>"+
-				"<li>Older browsers don't support the u or y flags</li>"+
-				"<li>Differences in handling of certain ambiguous escapes: \\8 \\9</li>"+
-				"<li>Chrome handles \\x & \\u escapes slightly differently than other browsers</li>"+
-				"<li>Chrome supports lookbehind, but it isn't yet in the JS spec</li>"+
-				"<li>Safari ignores leading zeros in octal escapes (ex. \\00020)</li>"+
+			desc:"您的浏览器的JavaScript引擎用于在异步工程中通过<code>RegExp.exec()</code>执行RegEx。"+
+				"<p>请注意，虽然实现大多是一致的，但浏览器之间存在微小差异。以下是已知的差异:<ul>"+
+				"<li>老的浏览器不支持u 或者 y 元字符</li>"+
+				"<li>在处理模棱两可的转义中的区别: \\8 \\9</li>"+
+				"<li>Chrome在处理 \\x & \\u 转义时，和其他浏览器有细微差别</li>"+
+				"<li>Chrome支持lookbehind, 但是它还没有在JS规范中</li>"+
+				"<li>Safari忽略了八进制转义中的前导零 (ex. \\00020)</li>"+
 				"</ul></p>"
 			},
 			{
 			label:"PCRE (PHP)",
-			desc:"PHP {{getPHPVersion()}} and PCRE {{getPCREVersion()}} are used to execute your pattern on our server."
+			desc:"PHP {{getPHPVersion()}} 和 PCRE {{getPCREVersion()}} 在我们的服务器端被用于执行您的模式。"
 			}
 		]
 		},
 		{
-		label:"Query string support",
-		desc:"In addition to the built in <b>Save</b> mechanism which creates a shareable link, RegExr also supports the ability to pre-populate a pattern via the query string."+
-			"<p>The following query string params are recognized:<ul>"+
-			"<li><code>engine</code> - the RegEx engine to use (<code>js</code> or <code>pcre</code>)</li>"+
-			"<li><code>expression</code> - populates the Expression area. It is recommended to pass a full expression with flags (<code>/.*/ig</code>) not just the pattern (<code>.*</code>)</li>"+
-			"<li><code>text</code> - populates the Text area</li>"+
-			"<li><code>tool</code> - sets the tool (replace, list, details, or explain)</li>"+
-			"<li><code>input</code> - populates the tool input field</li>"+
+		label:"支持字符串查询",
+		desc:"除了内置的<b>保存</b>机制，RegExr还可以创建可共享的链接。RegExr还支持通过查询字符串进行模式预填充。"+
+			"<p>字符串查询参数:<ul>"+
+			"<li><code>引擎</code> - RegEx引擎(<code>js</code> 或 <code>pcre</code>)</li>"+
+			"<li><code>表达式</code> - 填充表达式区域. 建议使用元字符传递完整表达式(<code>/.*/ig</code>)，而不仅仅是模式(<code>.*</code>)</li>"+
+			"<li><code>文本</code> - 填充文本区域</li>"+
+			"<li><code>工具</code> - 设置工具 (替换, 列表, 详细信息, 或 说明)</li>"+
+			"<li><code>输入</code> - 填充工具输入字段</li>"+
 			"</ul></p>"+
 			"Ex. <a href='http://regexr.com/?expression=/./g&text=test'>regexr.com/?expression=/./g&text=test</a>"
 		}
